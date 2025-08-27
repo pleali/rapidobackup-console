b32cb88f50066ddbd5ee50dfd8585ed6ff1facc8
pleali <pleali@rapidobackup.com>
2025-08-27 11:20:34 +0200

    refactor: Restore JPA configuration properties and beans in AgentJpaConfig

--- src/main/java/com/rapidobackup/console/agent/config/AgentJpaConfig.java
+++ src/main/java/com/rapidobackup/console/agent/config/AgentJpaConfig.java
@@ -1,8 +1,18 @@
 package com.rapidobackup.console.agent.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import jakarta.persistence.EntityManagerFactory;
 
 @Configuration
 @EnableTransactionManagement
@@ -11,35 +21,35 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 )
 public class AgentJpaConfig {
 
-    // @Value("${spring.datasource.url}")
-    // private String dbUrl;
+    @Value("${spring.datasource.url}")
+    private String dbUrl;
 
-    // @Value("${spring.datasource.username}")
-    // private String dbUsername;
+    @Value("${spring.datasource.username}")
+    private String dbUsername;
 
-    // @Value("${spring.datasource.password}")
-    // private String dbPassword;
+    @Value("${spring.datasource.password}")
+    private String dbPassword;
 
-    // @Bean
-    // public DataSource dataSource() {
-    //     return DataSourceBuilder.create()
-    //             .url(dbUrl)
-    //             .username(dbUsername)
-    //             .password(dbPassword)
-    //             .build();
-    // }
+    @Bean
+    public DataSource dataSource() {
+        return DataSourceBuilder.create()
+                .url(dbUrl)
+                .username(dbUsername)
+                .password(dbPassword)
+                .build();
+    }
 
-    // @Bean
-    // public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource) {
-    //     LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
-    //     em.setDataSource(dataSource);
-    //     em.setPackagesToScan("com.rapidobackup.console.agent.entity");
-    //     // Additional JPA properties can be set here
-    //     return em;
-    // }
+    @Bean(name = "entityManagerFactory")
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource) {
+        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+        em.setDataSource(dataSource);
+        em.setPackagesToScan("com.rapidobackup.console.agent.entity");
+        // Additional JPA properties can be set here
+        return em;
+    }
 
-    // @Bean
-    // public PlatformTransactionManager transactionManager(EntityManagerFactory emf) {
-    //     return new JpaTransactionManager(emf);
-    // }
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory emf) {
+        return new JpaTransactionManager(emf);
+    }
 }
