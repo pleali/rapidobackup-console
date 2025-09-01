d59987ba94987fb68879d71fac8459fd5904bd95
pleali <pleali@rapidobackup.com>
2025-09-01 19:06:26 +0200

    feat: Introduce CRLFLogConverter for log injection prevention and add application constants

--- src/main/java/com/rapidobackup/console/ConsoleApplication.java
+++ src/main/java/com/rapidobackup/console/ConsoleApplication.java
@@ -1,10 +1,22 @@
 package com.rapidobackup.console;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Optional;
+
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.core.env.Environment;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import com.rapidobackup.console.config.CRLFLogConverter;
+import com.rapidobackup.console.config.DefaultProfileUtil;
+
 
 /**
  * Main application class supporting both JPA (traditional modules) 
@@ -16,7 +28,54 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement
 public class ConsoleApplication {
 
+    private static final Logger LOG = LoggerFactory.getLogger(ConsoleApplication.class);
+
+    private final Environment env;
+
+    public ConsoleApplication(Environment env) {
+        this.env = env;
+    } 
+
   public static void main(String[] args) {
-    SpringApplication.run(ConsoleApplication.class, args);
+        SpringApplication app = new SpringApplication(ConsoleApplication.class);
+        DefaultProfileUtil.addDefaultProfile(app);
+        Environment env = app.run(args).getEnvironment();
+        logApplicationStartup(env);
+
   }
+  
+  private static void logApplicationStartup(Environment env) {
+        String protocol = Optional.ofNullable(env.getProperty("server.ssl.key-store")).map(key -> "https").orElse("http");
+        String applicationName = env.getProperty("spring.application.name");
+        String serverPort = env.getProperty("server.port");
+        String contextPath = Optional.ofNullable(env.getProperty("server.servlet.context-path"))
+            .filter(StringUtils::isNotBlank)
+            .orElse("/");
+        String hostAddress = "localhost";
+        try {
+            hostAddress = InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            LOG.warn("The host name could not be determined, using `localhost` as fallback");
+        }
+        LOG.info(
+            CRLFLogConverter.CRLF_SAFE_MARKER,
+            """
+
+            ----------------------------------------------------------
+            \tApplication '{}' is running! Access URLs:
+            \tLocal: \t\t{}://localhost:{}{}
+            \tExternal: \t{}://{}:{}{}
+            \tProfile(s): \t{}
+            ----------------------------------------------------------""",
+            applicationName,
+            protocol,
+            serverPort,
+            contextPath,
+            protocol,
+            hostAddress,
+            serverPort,
+            contextPath,
+            env.getActiveProfiles().length == 0 ? env.getDefaultProfiles() : env.getActiveProfiles()
+        );
+    }
 }
---
+++ src/main/java/com/rapidobackup/console/config/Constants.java
@@ -0,0 +1,38 @@
+package com.rapidobackup.console.config;
+
+/**
+ * Application constants.
+ */
+public interface Constants {
+
+    // Regex for acceptable logins
+    LOGIN_REGEX = "^(?>[a-zA-Z0-9!$&*+=?^_`{|}~.-]+@[a-zA-Z0-9-]+(?:\\.[a-zA-Z0-9-]+)*)|(?>[_.@A-Za-z0-9-]+)$";
+    SYSTEM = "system";
+    DEFAULT_LANGUAGE = "en";
+
+    // Spring profiles for development, test and production, 
+    /** Constant <code>SPRING_PROFILE_DEVELOPMENT="dev"</code> */
+    String SPRING_PROFILE_DEVELOPMENT = "dev";
+    /** Constant <code>SPRING_PROFILE_TEST="test"</code> */
+    String SPRING_PROFILE_TEST = "test";
+    /** Constant <code>SPRING_PROFILE_E2E="e2e"</code> */
+    String SPRING_PROFILE_E2E = "e2e";
+    /** Constant <code>SPRING_PROFILE_PRODUCTION="prod"</code> */
+    String SPRING_PROFILE_PRODUCTION = "prod";
+    /** Spring profile used when deploying with Spring Cloud (used when deploying to CloudFoundry)
+        Constant <code>SPRING_PROFILE_CLOUD="cloud"</code> */
+    String SPRING_PROFILE_CLOUD = "cloud";
+    /** Spring profile used when deploying to Heroku
+        Constant <code>SPRING_PROFILE_HEROKU="heroku"</code> */
+    String SPRING_PROFILE_HEROKU = "heroku";
+    /** Spring profile used to enable OpenAPI doc generation
+        Constant <code>SPRING_PROFILE_API_DOCS="api-docs"</code> */
+    String SPRING_PROFILE_API_DOCS = "api-docs";
+    /** Spring profile used to disable running liquibase
+        Constant <code>SPRING_PROFILE_NO_LIQUIBASE="no-liquibase"</code> */
+    String SPRING_PROFILE_NO_LIQUIBASE = "no-liquibase";
+    /** Spring profile used when deploying to Kubernetes and OpenShift
+        Constant <code>SPRING_PROFILE_K8S="k8s"</code> */
+    String SPRING_PROFILE_K8S = "k8s";
+
+}
