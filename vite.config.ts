8ea98b6d914b358f8d1420401904a4206c5a9ce2
pleali <pleali@rapidobackup.com>
2025-09-04 14:25:43 +0200

    fix: updated tailwind config

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src/main/webapp/src"),
     },
   },
-  root: path.resolve(__dirname,"src/main/webapp"),
-  publicDir: path.resolve(__dirname, "target/classes/static/public"),
-  cacheDir: path.resolve(__dirname,'target/.vite-cache'),
+  root: path.resolve(__dirname, "src/main/webapp"),
+  publicDir: "public",
+  cacheDir: path.resolve(__dirname, 'target/.vite-cache'),
 
   build: {
-    outDir: path.resolve(__dirname, "target/classes/static/public"),
+    outDir: path.resolve(__dirname, "target/classes/static"),
     rollupOptions: {
       input: {
         main: path.resolve(__dirname, 'src/main/webapp/index.html')
