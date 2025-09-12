0173ece9278c8fb2f637415720565d6c3bcff26f
pleali <pleali@rapidobackup.com>
2025-09-12 15:50:31 +0200

    fix: disable autocommit in jpa, remove agentJpa and blocking code

--- src/main/java/com/rapidobackup/console/config/JpaConfig.java
+++ src/main/java/com/rapidobackup/console/config/JpaConfig.java
@@ -1,11 +1,9 @@
 package com.rapidobackup.console.config;
 
-import java.util.Properties;
-
 import javax.sql.DataSource;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -16,67 +14,62 @@ import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 
+import com.zaxxer.hikari.HikariDataSource;
+
 import jakarta.persistence.EntityManagerFactory;
 
 /**
- * JPA configuration alongside R2DBC
- * Manually configures EntityManagerFactory for hybrid setup
+ * JPA configuration alongside R2DBC using HikariCP DataSource
+ * Explicitly configures HikariCP with auto-commit: false
  */
 @Configuration
 @EnableJpaRepositories(basePackages = {
     "com.rapidobackup.console.user.repository",
     "com.rapidobackup.console.auth.repository", 
-    "com.rapidobackup.console.backup.repository",
-    "com.rapidobackup.console.agent.repository"
+    "com.rapidobackup.console.backup.repository"
 }, entityManagerFactoryRef = "entityManagerFactory",
    transactionManagerRef = "transactionManager")
 @EnableJpaAuditing(auditorAwareRef = "springSecurityAuditorAware")
 public class JpaConfig {
 
-    @Value("${spring.datasource.url}")
-    private String url;
-    
-    @Value("${spring.datasource.username}")
-    private String username;
-    
-    @Value("${spring.datasource.password}")
-    private String password;
-    
     @Primary
     @Bean(name = "dataSource")
-    public DataSource dataSource() {
-        return DataSourceBuilder.create()
-                .url(url)
-                .username(username)
-                .password(password)
-                .build();
+    public DataSource dataSource(
+            @Value("${spring.datasource.url}") String url,
+            @Value("${spring.datasource.username}") String username,
+            @Value("${spring.datasource.password}") String password) {
+        
+        // Create HikariCP DataSource with explicit auto-commit configuration
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        
+        // Explicitly set auto-commit to false - this fixes the autocommit issue
+        dataSource.setAutoCommit(false);
+        dataSource.setPoolName("Hikari");
+        
+        return dataSource;
     }
 
     @Primary
     @Bean(name = "entityManagerFactory")
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
-        DataSource dataSource = dataSource();
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(
+            @Qualifier("dataSource") DataSource dataSource) {
+        
         LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
         factory.setDataSource(dataSource);
         factory.setPackagesToScan("com.rapidobackup.console");
         factory.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
         
-        Properties jpaProperties = new Properties();
-        jpaProperties.put("hibernate.hbm2ddl.auto", "validate");
-        jpaProperties.put("hibernate.show_sql", false);
-        jpaProperties.put("hibernate.format_sql", true);
-        jpaProperties.put("hibernate.jdbc.time_zone", "UTC");
-        jpaProperties.put("hibernate.id.new_generator_mappings", true);
-        jpaProperties.put("hibernate.connection.provider_disables_autocommit", true);
-        // Cache configuration removed - not essential for basic operation
-        
-        factory.setJpaProperties(jpaProperties);
         return factory;
     }
 
     @Primary
     @Bean(name = "transactionManager")
-    public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+    public PlatformTransactionManager transactionManager(
+            @Qualifier("entityManagerFactory") EntityManagerFactory entityManagerFactory) {
         return new JpaTransactionManager(entityManagerFactory);
     }
 }
