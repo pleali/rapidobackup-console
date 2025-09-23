c088fda984547908a49aa8c18d33231bad446d30
pleali <pleali@rapidobackup.com>
2025-09-23 09:17:20 +0200

    fix: The resource leak static initialization  for PostgreSQL test container

--- src/test/java/com/rapidobackup/console/AbstractIntegrationTest.java
+++ src/test/java/com/rapidobackup/console/AbstractIntegrationTest.java
@@ -2,6 +2,8 @@ package com.rapidobackup.console;
 
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -29,13 +31,11 @@ public abstract class AbstractIntegrationTest {
             .withPassword("test")
             .withReuse(true);
 
-    static {
-        postgresContainer.start();
-
-        // Set system properties for Spring to pick up
-        System.setProperty("spring.datasource.url", postgresContainer.getJdbcUrl());
-        System.setProperty("spring.datasource.username", postgresContainer.getUsername());
-        System.setProperty("spring.datasource.password", postgresContainer.getPassword());
-        System.setProperty("spring.datasource.driver-class-name", postgresContainer.getDriverClassName());
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgresContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", postgresContainer::getUsername);
+        registry.add("spring.datasource.password", postgresContainer::getPassword);
+        registry.add("spring.datasource.driver-class-name", postgresContainer::getDriverClassName);
     }
 }
